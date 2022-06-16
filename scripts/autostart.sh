da3301ba08493cefd3145d2901f3a5499e0b0737
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2022-06-16 11:54:24 -0500

    pulseaudio/pipewire
    options for pulseaudio and pipewire at autostart - options can be commented and uncommented for desired functionality

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-IMGs="$HOME/ashWM/img"
+BACKGROUNDS="$HOME/ashWM/img"
 xscreensaver -nosplash &
-nitrogen --random $IMGs &
+nitrogen --random $BACKGROUNDS &
 conky &
 polkit &
 polkit-dumb-agent &
 dunst &
 xscreensaver -no-splash & 
+# AUDIO SETTINGS
+#
+# On nightshade, we are using PulseAudio with pamixer. 
+#
+# Many users prefer Pipewire these days, so the option is here for both. 
+# 
+# You can uncomment the option you would like to use so it is active - just be sure to comment the old one out. 
+# pipewire &
+pulseaudio &
 dwm
